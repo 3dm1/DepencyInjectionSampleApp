835225af53040b996a7312cb4c1bc0ed932cb6c1
Edson Menegatti <edson@ckl.io>
2016-02-28 17:26:44 -0300

    Create AppModule

---
+++ app/src/main/java/io/ckl/depencyinjectionsampleapp/dagger/modules/AppModule.java
@@ -0,0 +1,35 @@
+package io.ckl.depencyinjectionsampleapp.dagger.modules;
+
+import android.content.Context;
+
+import dagger.Module;
+import dagger.Provides;
+import io.ckl.depencyinjectionsampleapp.dagger.scope.PerApp;
+import io.ckl.depencyinjectionsampleapp.data.api.GitHubService;
+import io.ckl.depencyinjectionsampleapp.data.model.GitHubUserModel;
+
+/**
+ * Created by edsonmenegatti on 2/28/16.
+ */
+@PerApp
+@Module
+public class AppModule {
+
+	private final Context mApplicationContext;
+
+	public AppModule(Context applicationContext) {
+		mApplicationContext = applicationContext;
+	}
+
+	@Provides
+	@PerApp
+	public Context provideApplicationContext() {
+		return mApplicationContext;
+	}
+
+	@Provides
+	@PerApp
+	public GitHubUserModel provideGitHubUserModel(GitHubService service) {
+		return new GitHubUserModel(service);
+	}
+}
